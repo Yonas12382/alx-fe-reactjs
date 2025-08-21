6d1b6e62e01d912ae509feb9d6a74cb5fb2e1040
YONAS ABADI GEBREMARIAM <Yonatanabadi12@gmail.com>
2025-08-21 18:07:46 +0300

    Create MainContent.jsx

---
+++ alx-react-app-new/MainContent.jsx
@@ -0,0 +1,12 @@
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#f9f9f9' }}>
+      <h2 style={{ color: 'darkgreen' }}>Welcome to my profile!</h2>
+      <p style={{ fontSize: '16px', lineHeight: '1.5' }}>
+        I love traveling and exploring different cultures around the world.
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
